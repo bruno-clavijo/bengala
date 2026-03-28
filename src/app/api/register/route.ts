4ac6c7273ecc9f3aca44d4dcb27020f0332c4e4a
bruno-clavijo <52514767+bruno-clavijo@users.noreply.github.com>
2026-03-28 16:06:39 -0600

    Update route.ts

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -18,7 +18,8 @@ export async function POST(req: Request) {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
+      database: process.env.DB_NAME,
+      port: Number(process.env.DB_PORT)
     });
 
     await connection.execute(
